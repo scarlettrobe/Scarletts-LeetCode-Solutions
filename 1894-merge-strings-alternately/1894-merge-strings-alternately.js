101ae844df56886b1bee5348df3729d7667cefcc
Kim Harris <91627325+scarlettrobe@users.noreply.github.com>
2023-09-01 23:37:48 -0500

    Time: 59 ms (35.31%), Space: 42.2 MB (51.62%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    result = '';
+    let i = 0 ;
+    let j = 0;
+    while ( i < word1.length && j < word2.length){
+        result += word1[i];
+        result += word2[j];
+        i++
+        j++
+    }
+
+while(i<word1.length){
+    result += word1[i];
+    i++;
+
+}
+
+while (j < word2.length){
+    result += word2[j];
+    j++;
+}
+return result;
+};
